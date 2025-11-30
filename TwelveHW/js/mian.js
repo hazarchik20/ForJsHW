fc47698a83bc77acc178c5f14dc7c148540efbe4
hazarchik20 <nazariypan2702@gmail.com>
2025-11-30 14:03:18 +0200

    12 hw

---
+++ TwelveHW/js/mian.js
@@ -0,0 +1,37 @@
+function randomTime() {
+    return Math.floor(Math.random() * 3000) + 1000;
+}
+function loadImages() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Завантажено картинки"), randomTime());
+    });
+}
+function loadProducts() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Завантажено продукти"), randomTime());
+    });
+}
+function loadUsers() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Завантажено користувачів"), randomTime());
+    });
+}
+loadImages()
+    .then(result => {
+        console.log(result);
+        return loadProducts();
+    })
+    .then(result => {
+        console.log(result);
+        return loadUsers();
+    })
+    .then(result => {
+        console.log(result);
+        console.log("=== Послідовне виконання завершено ===");
+    });
+
+Promise.all([loadImages(), loadProducts(), loadUsers()])
+    .then(results => {
+        console.log(results);
+        console.log("=== Одночасне виконання завершено ===");
+    });
